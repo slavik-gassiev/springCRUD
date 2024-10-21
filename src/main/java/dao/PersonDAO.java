130bd7a74d6f916998533217d730f6023ff3876a
slavik-gassiev <slavikgassiev1872002@gmail.com>
2024-10-21 18:20:16 +0300

    Added hibernate validation

--- src/main/java/dao/PersonDAO.java
+++ src/main/java/dao/PersonDAO.java
@@ -13,10 +13,11 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
 
     public List<Person> index() {
@@ -32,12 +33,15 @@ public class PersonDAO {
         people.add(person);
     }
 
-    public void update( int id, Person updatedPerson) {
+    public void update(int id, Person updatedPerson) {
         Person personToBeUpdated = show(id);
+
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(person -> person.getId() == id);
+        people.removeIf(p -> p.getId() == id);
     }
 }
